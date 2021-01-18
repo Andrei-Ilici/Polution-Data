7f00d59646c1a42f3a92879194d43fe9b1825adb
Andrei-Ilici <andrei.ilici29@gmail.com>
2021-01-18 00:29:34 +0000

    3

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from urllib.request import urlopen
 from bucket import connect_to_s3, list_s3_objects
 from database import print_vers, connect_to_db, create_database, use_database, create_uk_table, create_world_table
 
-try: 
-    conn = connect_to_db()
-    cursor = conn.cursor()
-except Exception as ERROR:
-    print('problem is '+ str(ERROR))
+
+conn = connect_to_db()
+cursor = conn.cursor()
+
+print('problem is ')
 print_vers(cursor)
 
 s3 = connect_to_s3()
